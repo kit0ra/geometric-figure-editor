e95e6b17ca01791c2d508fa4cb7b2670aa2d03c4
kit0ra <noovaomega@gmail.com>
2025-04-05 18:38:40 +0200

    fixed project's entry point

--- src/main/java/com/geometriceditor/App.java
+++ src/main/java/com/geometriceditor/App.java
@@ -1,11 +1,19 @@
 package com.geometriceditor;
 
+import javax.swing.SwingUtilities;
+
+import com.geometriceditor.ui.MainWindow;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Ensure UI is created on Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            MainWindow window = new MainWindow();
+            window.setVisible(true);
+        });
     }
 }
